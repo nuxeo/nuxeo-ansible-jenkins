21d84ec605d911b3c633ea37d663b3563fbb1cd2
Mathieu Guillaume <mg@nuxeo.com>
2017-10-17 11:11:38 +0200

    Quote values in enbv init file

--- roles/slave_tools/files/10_setup_environment.sh
+++ roles/slave_tools/files/10_setup_environment.sh
@@ -32,14 +32,14 @@ echo "DISPLAY=:1" >>  /etc/environment
 for v in HOST PORT NAME USER PASS ADMINNAME ADMINUSER ADMINPASS; do
     var="NX_DB_${v}"
     if [ "${!var}x" != "x" ]; then
-        echo ${!var} >> /etc/container_environment/${var}
-        echo "${var}=${!var}" >> /etc/environment
+        echo ${!var} > /etc/container_environment/${var}
+        echo "${var}=\"${!var}\"" >> /etc/environment
     fi
     for d in MSSQL MYSQL MARIADB ORACLE11G ORACLE12C PGSQL; do
         dbvar="NX_${d}_DB_${v}"
         if [ "${!dbvar}x" != "x" ]; then
-            echo ${!dbvar} >> /etc/container_environment/${dbvar}
-            echo "${dbvar}=${!dbvar}" >> /etc/environment
+            echo ${!dbvar} > /etc/container_environment/${dbvar}
+            echo "${dbvar}=\"${!dbvar}\"" >> /etc/environment
         fi
     done
 done
@@ -48,8 +48,8 @@ done
 for v in SERVER DBNAME; do
     var="NX_MONGODB_$v"
     if [ "${!var}x" != "x" ]; then
-        echo ${!var} >> /etc/container_environment/NX_MONGODB_$v
-        echo "NX_MONGODB_$v=${!var}" >> /etc/environment
+        echo ${!var} > /etc/container_environment/NX_MONGODB_$v
+        echo "NX_MONGODB_$v=\"${!var}\"" >> /etc/environment
     fi
 done
 
@@ -59,70 +59,70 @@ if [ -z "${MULTIDB}" ]; then
 
 
     if [ ! -f /etc/container_environment/NX_DB_PASS ]; then
-        echo pwd${SLAVE_NAME} >> /etc/container_environment/NX_DB_PASS
-        echo "NX_DB_PASS=pwd${SLAVE_NAME}" >> /etc/environment
+        echo pwd${SLAVE_NAME} > /etc/container_environment/NX_DB_PASS
+        echo "NX_DB_PASS=\"pwd${SLAVE_NAME}\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_DB_USER ]; then
-        echo user${SLAVE_NAME} >> /etc/container_environment/NX_DB_USER
-        echo "NX_DB_USER=user${SLAVE_NAME}" >> /etc/environment
+        echo user${SLAVE_NAME} > /etc/container_environment/NX_DB_USER
+        echo "NX_DB_USER=\"user${SLAVE_NAME}\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_DB_HOST ]; then
-        echo "127.0.0.1" >> /etc/container_environment/NX_DB_HOST
-        echo "NX_DB_HOST=127.0.0.1" >> /etc/environment
+        echo "127.0.0.1" > /etc/container_environment/NX_DB_HOST
+        echo "NX_DB_HOST=\"127.0.0.1\"" >> /etc/environment
     fi
 
 
     if [ ! -f /etc/container_environment/NX_MYSQL_DB_ADMINNAME ]; then
-        echo mysql >> /etc/container_environment/NX_MYSQL_DB_ADMINNAME
-        echo "NX_MYSQL_DB_ADMINNAME=mysql" >> /etc/environment
+        echo mysql > /etc/container_environment/NX_MYSQL_DB_ADMINNAME
+        echo "NX_MYSQL_DB_ADMINNAME=\"mysql\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MYSQL_DB_ADMINPASS ]; then
-        echo nuxeo >> /etc/container_environment/NX_MYSQL_DB_ADMINPASS
-        echo "NX_MYSQL_DB_ADMINPASS=nuxeo" >> /etc/environment
+        echo nuxeo > /etc/container_environment/NX_MYSQL_DB_ADMINPASS
+        echo "NX_MYSQL_DB_ADMINPASS=\"nuxeo\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MYSQL_DB_ADMINUSER ]; then
-        echo root >> /etc/container_environment/NX_MYSQL_DB_ADMINUSER
-        echo "NX_MYSQL_DB_ADMINUSER=root" >> /etc/environment
+        echo root > /etc/container_environment/NX_MYSQL_DB_ADMINUSER
+        echo "NX_MYSQL_DB_ADMINUSER=\"root\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MYSQL_DB_NAME ]; then
-        echo db${SLAVE_NAME} >> /etc/container_environment/NX_MYSQL_DB_NAME
-        echo "NX_MYSQL_DB_NAME=db${SLAVE_NAME}" >> /etc/environment
+        echo db${SLAVE_NAME} > /etc/container_environment/NX_MYSQL_DB_NAME
+        echo "NX_MYSQL_DB_NAME=\"db${SLAVE_NAME}\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MYSQL_DB_PORT ]; then
-        echo 3306 >> /etc/container_environment/NX_MYSQL_DB_PORT
-        echo "NX_MYSQL_DB_PORT=3306" >> /etc/environment
+        echo 3306 > /etc/container_environment/NX_MYSQL_DB_PORT
+        echo "NX_MYSQL_DB_PORT=\"3306\"" >> /etc/environment
     fi
 
 
     if [ ! -f /etc/container_environment/NX_PGSQL_DB_ADMINNAME ]; then
-        echo template1 >> /etc/container_environment/NX_PGSQL_DB_ADMINNAME
-        echo "NX_PGSQL_DB_ADMINNAME=template1" >> /etc/environment
+        echo template1 > /etc/container_environment/NX_PGSQL_DB_ADMINNAME
+        echo "NX_PGSQL_DB_ADMINNAME=\"template1\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_PGSQL_DB_ADMINPASS ]; then
-        echo nuxeo >> /etc/container_environment/NX_PGSQL_DB_ADMINPASS
-        echo "NX_PGSQL_DB_ADMINPASS=nuxeo" >> /etc/environment
+        echo nuxeo > /etc/container_environment/NX_PGSQL_DB_ADMINPASS
+        echo "NX_PGSQL_DB_ADMINPASS=\"nuxeo\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_PGSQL_DB_ADMINUSER ]; then
-        echo nxadmin >> /etc/container_environment/NX_PGSQL_DB_ADMINUSER
-        echo "NX_PGSQL_DB_ADMINUSER=nxadmin" >> /etc/environment
+        echo nxadmin > /etc/container_environment/NX_PGSQL_DB_ADMINUSER
+        echo "NX_PGSQL_DB_ADMINUSER=\"nxadmin\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_PGSQL_DB_NAME ]; then
-        echo db${SLAVE_NAME} >> /etc/container_environment/NX_PGSQL_DB_NAME
-        echo "NX_PGSQL_DB_NAME=db${SLAVE_NAME}" >> /etc/environment
+        echo db${SLAVE_NAME} > /etc/container_environment/NX_PGSQL_DB_NAME
+        echo "NX_PGSQL_DB_NAME=\"db${SLAVE_NAME}\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_PGSQL_DB_PORT ]; then
-        echo 5432 >> /etc/container_environment/NX_PGSQL_DB_PORT
-        echo "NX_PGSQL_DB_PORT=5432" >> /etc/environment
+        echo 5432 > /etc/container_environment/NX_PGSQL_DB_PORT
+        echo "NX_PGSQL_DB_PORT=\"5432\"" >> /etc/environment
     fi
 
 
     if [ ! -f /etc/container_environment/NX_MONGODB_SERVER ]; then
-        echo "127.0.0.1" >> /etc/container_environment/NX_MONGODB_SERVER
-        echo "NX_MONGODB_SERVER=127.0.0.1" >> /etc/environment
+        echo "127.0.0.1" > /etc/container_environment/NX_MONGODB_SERVER
+        echo "NX_MONGODB_SERVER=\"127.0.0.1\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MONGODB_DBNAME ]; then
-        echo db${SLAVE_NAME} >> /etc/container_environment/NX_MONGODB_DBNAME
-        echo "NX_MONGODB_DBNAME=db${SLAVE_NAME}" >> /etc/environment
+        echo db${SLAVE_NAME} > /etc/container_environment/NX_MONGODB_DBNAME
+        echo "NX_MONGODB_DBNAME=\"db${SLAVE_NAME}\"" >> /etc/environment
     fi
 
 
@@ -130,157 +130,157 @@ else
 
 
     if [ ! -f /etc/container_environment/NX_DB_PASS ]; then
-        echo pwd${SLAVE_NAME} >> /etc/container_environment/NX_DB_PASS
-        echo "NX_DB_PASS=pwd${SLAVE_NAME}" >> /etc/environment
+        echo pwd${SLAVE_NAME} > /etc/container_environment/NX_DB_PASS
+        echo "NX_DB_PASS=\"pwd${SLAVE_NAME}\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_DB_USER ]; then
-        echo user${SLAVE_NAME} >> /etc/container_environment/NX_DB_USER
-        echo "NX_DB_USER=user${SLAVE_NAME}" >> /etc/environment
+        echo user${SLAVE_NAME} > /etc/container_environment/NX_DB_USER
+        echo "NX_DB_USER=\"user${SLAVE_NAME}\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_DB_HOST ]; then
-        echo ${HOST_IP} >> /etc/container_environment/NX_DB_HOST
-        echo "NX_DB_HOST=${HOST_IP}" >> /etc/environment
+        echo ${HOST_IP} > /etc/container_environment/NX_DB_HOST
+        echo "NX_DB_HOST=\"${HOST_IP}\"" >> /etc/environment
     fi
 
 
     if [ ! -f /etc/container_environment/NX_MSSQL_DB_ADMINNAME ]; then
-        echo master >> /etc/container_environment/NX_MSSQL_DB_ADMINNAME
-        echo "NX_MSSQL_DB_ADMINNAME=master" >> /etc/environment
+        echo master > /etc/container_environment/NX_MSSQL_DB_ADMINNAME
+        echo "NX_MSSQL_DB_ADMINNAME=\"master\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MSSQL_DB_ADMINPASS ]; then
-        echo nuxeo >> /etc/container_environment/NX_MSSQL_DB_ADMINPASS
-        echo "NX_MSSQL_DB_ADMINPASS=nuxeo" >> /etc/environment
+        echo nuxeo > /etc/container_environment/NX_MSSQL_DB_ADMINPASS
+        echo "NX_MSSQL_DB_ADMINPASS=\"nuxeo\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MSSQL_DB_ADMINUSER ]; then
-        echo sa >> /etc/container_environment/NX_MSSQL_DB_ADMINUSER
-        echo "NX_MSSQL_DB_ADMINUSER=sa" >> /etc/environment
+        echo sa > /etc/container_environment/NX_MSSQL_DB_ADMINUSER
+        echo "NX_MSSQL_DB_ADMINUSER=\"sa\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MSSQL_DB_NAME ]; then
-        echo db${SLAVE_NAME} >> /etc/container_environment/NX_MSSQL_DB_NAME
-        echo "NX_MSSQL_DB_NAME=db${SLAVE_NAME}" >> /etc/environment
+        echo db${SLAVE_NAME} > /etc/container_environment/NX_MSSQL_DB_NAME
+        echo "NX_MSSQL_DB_NAME=\"db${SLAVE_NAME}\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MSSQL_DB_PORT ]; then
-        echo 1433 >> /etc/container_environment/NX_MSSQL_DB_PORT
-        echo "NX_MSSQL_DB_PORT=1433" >> /etc/environment
+        echo 1433 > /etc/container_environment/NX_MSSQL_DB_PORT
+        echo "NX_MSSQL_DB_PORT=\"1433\"" >> /etc/environment
     fi
 
     if [ ! -f /etc/container_environment/NX_MYSQL_DB_ADMINNAME ]; then
-        echo mysql >> /etc/container_environment/NX_MYSQL_DB_ADMINNAME
-        echo "NX_MYSQL_DB_ADMINNAME=mysql" >> /etc/environment
+        echo mysql > /etc/container_environment/NX_MYSQL_DB_ADMINNAME
+        echo "NX_MYSQL_DB_ADMINNAME=\"mysql\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MYSQL_DB_ADMINPASS ]; then
-        echo nuxeo >> /etc/container_environment/NX_MYSQL_DB_ADMINPASS
-        echo "NX_MYSQL_DB_ADMINPASS=nuxeo" >> /etc/environment
+        echo nuxeo > /etc/container_environment/NX_MYSQL_DB_ADMINPASS
+        echo "NX_MYSQL_DB_ADMINPASS=\"nuxeo\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MYSQL_DB_ADMINUSER ]; then
-        echo root >> /etc/container_environment/NX_MYSQL_DB_ADMINUSER
-        echo "NX_MYSQL_DB_ADMINUSER=root" >> /etc/environment
+        echo root > /etc/container_environment/NX_MYSQL_DB_ADMINUSER
+        echo "NX_MYSQL_DB_ADMINUSER=\"root\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MYSQL_DB_NAME ]; then
-        echo db${SLAVE_NAME} >> /etc/container_environment/NX_MYSQL_DB_NAME
-        echo "NX_MYSQL_DB_NAME=db${SLAVE_NAME}" >> /etc/environment
+        echo db${SLAVE_NAME} > /etc/container_environment/NX_MYSQL_DB_NAME
+        echo "NX_MYSQL_DB_NAME=\"db${SLAVE_NAME}\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MYSQL_DB_PORT ]; then
-        echo 3306 >> /etc/container_environment/NX_MYSQL_DB_PORT
-        echo "NX_MYSQL_DB_PORT=3306" >> /etc/environment
+        echo 3306 > /etc/container_environment/NX_MYSQL_DB_PORT
+        echo "NX_MYSQL_DB_PORT=\"3306\"" >> /etc/environment
     fi
 
 
     if [ ! -f /etc/container_environment/NX_MARIADB_DB_ADMINNAME ]; then
-        echo mysql >> /etc/container_environment/NX_MARIADB_DB_ADMINNAME
-        echo "NX_MARIADB_DB_ADMINNAME=mysql" >> /etc/environment
+        echo mysql > /etc/container_environment/NX_MARIADB_DB_ADMINNAME
+        echo "NX_MARIADB_DB_ADMINNAME=\"mysql\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MARIADB_DB_ADMINPASS ]; then
-        echo nuxeo >> /etc/container_environment/NX_MARIADB_DB_ADMINPASS
-        echo "NX_MARIADB_DB_ADMINPASS=nuxeo" >> /etc/environment
+        echo nuxeo > /etc/container_environment/NX_MARIADB_DB_ADMINPASS
+        echo "NX_MARIADB_DB_ADMINPASS=\"nuxeo\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MARIADB_DB_ADMINUSER ]; then
-        echo root >> /etc/container_environment/NX_MARIADB_DB_ADMINUSER
-        echo "NX_MARIADB_DB_ADMINUSER=root" >> /etc/environment
+        echo root > /etc/container_environment/NX_MARIADB_DB_ADMINUSER
+        echo "NX_MARIADB_DB_ADMINUSER=\"root\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MARIADB_DB_NAME ]; then
-        echo db${SLAVE_NAME} >> /etc/container_environment/NX_MARIADB_DB_NAME
-        echo "NX_MARIADB_DB_NAME=db${SLAVE_NAME}" >> /etc/environment
+        echo db${SLAVE_NAME} > /etc/container_environment/NX_MARIADB_DB_NAME
+        echo "NX_MARIADB_DB_NAME=\"db${SLAVE_NAME}\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MARIADB_DB_PORT ]; then
-        echo 3307 >> /etc/container_environment/NX_MARIADB_DB_PORT
-        echo "NX_MARIADB_DB_PORT=3307" >> /etc/environment
+        echo 3307 > /etc/container_environment/NX_MARIADB_DB_PORT
+        echo "NX_MARIADB_DB_PORT=\"3307\"" >> /etc/environment
     fi
 
 
     if [ ! -f /etc/container_environment/NX_ORACLE11G_DB_ADMINNAME ]; then
-        echo nuxeo >> /etc/container_environment/NX_ORACLE11G_DB_ADMINNAME
-        echo "NX_ORACLE11G_DB_ADMINNAME=nuxeo" >> /etc/environment
+        echo nuxeo > /etc/container_environment/NX_ORACLE11G_DB_ADMINNAME
+        echo "NX_ORACLE11G_DB_ADMINNAME=\"nuxeo\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_ORACLE11G_DB_ADMINPASS ]; then
-        echo nuxeo >> /etc/container_environment/NX_ORACLE11G_DB_ADMINPASS
-        echo "NX_ORACLE11G_DB_ADMINPASS=nuxeo" >> /etc/environment
+        echo nuxeo > /etc/container_environment/NX_ORACLE11G_DB_ADMINPASS
+        echo "NX_ORACLE11G_DB_ADMINPASS=\"nuxeo\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_ORACLE11G_DB_ADMINUSER ]; then
-        echo sys >> /etc/container_environment/NX_ORACLE11G_DB_ADMINUSER
-        echo "NX_ORACLE11G_DB_ADMINUSER=sys" >> /etc/environment
+        echo sys > /etc/container_environment/NX_ORACLE11G_DB_ADMINUSER
+        echo "NX_ORACLE11G_DB_ADMINUSER=\"sys\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_ORACLE11G_DB_NAME ]; then
-        echo nuxeo >> /etc/container_environment/NX_ORACLE11G_DB_NAME
-        echo "NX_ORACLE11G_DB_NAME=nuxeo" >> /etc/environment
+        echo nuxeo > /etc/container_environment/NX_ORACLE11G_DB_NAME
+        echo "NX_ORACLE11G_DB_NAME=\"nuxeo\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_ORACLE11G_DB_PORT ]; then
-        echo 1520 >> /etc/container_environment/NX_ORACLE11G_DB_PORT
-        echo "NX_ORACLE11G_DB_PORT=1520" >> /etc/environment
+        echo 1520 > /etc/container_environment/NX_ORACLE11G_DB_PORT
+        echo "NX_ORACLE11G_DB_PORT=\"1520\"" >> /etc/environment
     fi
 
 
     if [ ! -f /etc/container_environment/NX_ORACLE12C_DB_ADMINNAME ]; then
-        echo nuxeo >> /etc/container_environment/NX_ORACLE12C_DB_ADMINNAME
-        echo "NX_ORACLE12C_DB_ADMINNAME=nuxeo" >> /etc/environment
+        echo nuxeo > /etc/container_environment/NX_ORACLE12C_DB_ADMINNAME
+        echo "NX_ORACLE12C_DB_ADMINNAME=\"nuxeo\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_ORACLE12C_DB_ADMINPASS ]; then
-        echo nuxeo >> /etc/container_environment/NX_ORACLE12C_DB_ADMINPASS
-        echo "NX_ORACLE12C_DB_ADMINPASS=nuxeo" >> /etc/environment
+        echo nuxeo > /etc/container_environment/NX_ORACLE12C_DB_ADMINPASS
+        echo "NX_ORACLE12C_DB_ADMINPASS=\"nuxeo\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_ORACLE12C_DB_ADMINUSER ]; then
-        echo sys >> /etc/container_environment/NX_ORACLE12C_DB_ADMINUSER
-        echo "NX_ORACLE12C_DB_ADMINUSER=sys" >> /etc/environment
+        echo sys > /etc/container_environment/NX_ORACLE12C_DB_ADMINUSER
+        echo "NX_ORACLE12C_DB_ADMINUSER=\"sys\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_ORACLE12C_DB_NAME ]; then
-        echo nuxeo >> /etc/container_environment/NX_ORACLE12C_DB_NAME
-        echo "NX_ORACLE12C_DB_NAME=nuxeo" >> /etc/environment
+        echo nuxeo > /etc/container_environment/NX_ORACLE12C_DB_NAME
+        echo "NX_ORACLE12C_DB_NAME=\"nuxeo\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_ORACLE12C_DB_PORT ]; then
-        echo 1521 >> /etc/container_environment/NX_ORACLE12C_DB_PORT
-        echo "NX_ORACLE12C_DB_PORT=1521" >> /etc/environment
+        echo 1521 > /etc/container_environment/NX_ORACLE12C_DB_PORT
+        echo "NX_ORACLE12C_DB_PORT=\"1521\"" >> /etc/environment
     fi
 
 
     if [ ! -f /etc/container_environment/NX_PGSQL_DB_ADMINNAME ]; then
-        echo template1 >> /etc/container_environment/NX_PGSQL_DB_ADMINNAME
-        echo "NX_PGSQL_DB_ADMINNAME=template1" >> /etc/environment
+        echo template1 > /etc/container_environment/NX_PGSQL_DB_ADMINNAME
+        echo "NX_PGSQL_DB_ADMINNAME=\"template1\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_PGSQL_DB_ADMINPASS ]; then
-        echo nuxeo >> /etc/container_environment/NX_PGSQL_DB_ADMINPASS
-        echo "NX_PGSQL_DB_ADMINPASS=nuxeo" >> /etc/environment
+        echo nuxeo > /etc/container_environment/NX_PGSQL_DB_ADMINPASS
+        echo "NX_PGSQL_DB_ADMINPASS=\"nuxeo\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_PGSQL_DB_ADMINUSER ]; then
-        echo nxadmin >> /etc/container_environment/NX_PGSQL_DB_ADMINUSER
-        echo "NX_PGSQL_DB_ADMINUSER=nxadmin" >> /etc/environment
+        echo nxadmin > /etc/container_environment/NX_PGSQL_DB_ADMINUSER
+        echo "NX_PGSQL_DB_ADMINUSER=\"nxadmin\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_PGSQL_DB_NAME ]; then
-        echo db${SLAVE_NAME} >> /etc/container_environment/NX_PGSQL_DB_NAME
-        echo "NX_PGSQL_DB_NAME=db${SLAVE_NAME}" >> /etc/environment
+        echo db${SLAVE_NAME} > /etc/container_environment/NX_PGSQL_DB_NAME
+        echo "NX_PGSQL_DB_NAME=\"db${SLAVE_NAME}\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_PGSQL_DB_PORT ]; then
-        echo 5432 >> /etc/container_environment/NX_PGSQL_DB_PORT
-        echo "NX_PGSQL_DB_PORT=5432" >> /etc/environment
+        echo 5432 > /etc/container_environment/NX_PGSQL_DB_PORT
+        echo "NX_PGSQL_DB_PORT=\"5432\"" >> /etc/environment
     fi
 
 
     if [ ! -f /etc/container_environment/NX_MONGODB_SERVER ]; then
-        echo ${HOST_IP} >> /etc/container_environment/NX_MONGODB_SERVER
-        echo "NX_MONGODB_SERVER=${HOST_IP}" >> /etc/environment
+        echo ${HOST_IP} > /etc/container_environment/NX_MONGODB_SERVER
+        echo "NX_MONGODB_SERVER=\"${HOST_IP}\"" >> /etc/environment
     fi
     if [ ! -f /etc/container_environment/NX_MONGODB_DBNAME ]; then
-        echo db${SLAVE_NAME} >> /etc/container_environment/NX_MONGODB_DBNAME
-        echo "NX_MONGODB_DBNAME=db${SLAVE_NAME}" >> /etc/environment
+        echo db${SLAVE_NAME} > /etc/container_environment/NX_MONGODB_DBNAME
+        echo "NX_MONGODB_DBNAME=\"db${SLAVE_NAME}\"" >> /etc/environment
     fi
 
 
