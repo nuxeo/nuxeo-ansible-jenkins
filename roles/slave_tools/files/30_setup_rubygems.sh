aa86fbdf35eed7d3c9a89d09787b2f608e11806b
Thomas Roger <troger@nuxeo.com>
2018-08-24 12:28:35 +0200

    NXBT-2432: use /opt/jenkins/.ssh/environment file to configure the GEM_HOME variable

---
+++ roles/slave_tools/files/30_setup_rubygems.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+# add user installed gems bin to PATH
+if which ruby >/dev/null && which gem >/dev/null; then
+
+    sudo -H -u jenkins sh <<"EOF"
+GEM_HOME="$(ruby -rubygems -e 'puts Gem.user_dir')"
+echo "GEM_HOME=$GEM_HOME" >>  $HOME/.ssh/environment
+echo "PATH=$GEM_HOME/bin:$PATH" >>  $HOME/.ssh/environment
+EOF
+
+fi
