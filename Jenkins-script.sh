a74d164021eab3776f813599e30e78893de881c3
alexistimic <atimic@nuxeo.com>
2018-11-21 10:14:06 +0100

    NXBT-2577: Update Jenkins bash scripts

--- Jenkins-script.sh
+++ Jenkins-script.sh
@@ -1,12 +1,13 @@
-#!/bin/bash -xe
+#!/bin/bash -ex
 
 docker login dockerpriv.nuxeo.com:443 -u $JUSER -p $JPASS
 
 echo CLEANUP
-docker kill slave-common-preprod slave-pub-preprod slave-priv-preprod || true
-docker rm -v slave-common-preprod slave-pub-preprod slave-priv-preprod || true
+docker kill slave-common slave-pub slave-priv slave-preprod|| true
+docker rm -v slave-common slave-pub slave-priv slave-preprod|| true
 
 docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv || true
+docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepreprod || true
 docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-check || true
 docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-ondemand || true
 docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-multidb || true
@@ -14,19 +15,20 @@ docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-it || true
 docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-itpriv || true
 docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-slave || true
 docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-swarm || true
-docker rmi nuxeo/jenkins-priv:preprod || true
-docker rmi nuxeo/jenkins-pub:preprod || true
-docker rmi nuxeo/jenkins-common:preprod || true
-docker rmi nuxeo/jenkins-base:preprod || true
-docker rmi nuxeo/jenkins-swarm:preprod || true
-docker images -a -q |xargs docker rmi -f || true
-docker image prune -f
+docker rmi nuxeo/jenkins-priv || true
+docker rmi nuxeo/jenkins-preprod || true
+docker rmi nuxeo/jenkins-pub || true
+docker rmi nuxeo/jenkins-common || true
+docker rmi nuxeo/jenkins-base || true
+docker rmi nuxeo/jenkins-swarm || true
+docker images dockerpriv.nuxeo.com:443/nuxeo/* -f "dangling=true" -q | sort -u | xargs -r docker rmi -f
+
 
 echo BUILDING jenkins-base...
-cp ~/.ssh/id_rsa.pub docker/base/files/id_rsa.pub
-docker build -t nuxeo/jenkins-base:preprod docker/base
+cp ~/.ssh/id_rsa.pub docker/base/files/
+docker build -t nuxeo/jenkins-base docker/base
 
-echo BUILDING preprod slaves...
+echo BUILDING slaves
 rm -rf venv
 virtualenv venv
 . venv/bin/activate
@@ -36,39 +38,65 @@ pip install ansible==2.2.0
 pip install boto
 pip install --upgrade awscli
 
-docker run -d -t -i --add-host unicode.org:164.132.165.173 -p 127.0.0.1:3232:22 --name=slave-common-preprod nuxeo/jenkins-base:preprod
-ansible-playbook -i inventory/slave-common/hosts -e "ssh_port=3232" --ssh-extra-args="-p 3232" slave-common.yml
-docker commit slave-common-preprod nuxeo/jenkins-common:preprod
-docker kill slave-common-preprod
-docker rm -v slave-common-preprod
+docker run -d -t -i --add-host unicode.org:164.132.165.173 -p 127.0.0.1:2222:22 --name=slave-common nuxeo/jenkins-base
+ansible-playbook -i inventory/slave-common/hosts slave-common.yml -v
+docker commit slave-common nuxeo/jenkins-common
+docker kill slave-common
+docker rm -v slave-common
 
-docker run -d -t -i --add-host unicode.org:164.132.165.173 -p 127.0.0.1:4232:22 --name=slave-pub-preprod nuxeo/jenkins-common:preprod
-ansible-playbook -i inventory/slave-pub/hosts -e "ssh_port=4232" --ssh-extra-args="-p 4232" slave-pub.yml -v
-docker commit slave-pub-preprod nuxeo/jenkins-pub:preprod
-docker kill slave-pub-preprod
-docker rm -v slave-pub-preprod
+docker run -d -t -i --add-host unicode.org:164.132.165.173 -p 127.0.0.1:2223:22 --name=slave-pub nuxeo/jenkins-common
+ansible-playbook -i inventory/slave-pub/hosts slave-pub.yml -v
+docker commit slave-pub nuxeo/jenkins-pub
+docker kill slave-pub
+docker rm -v slave-pub
 
-docker run -d -t -i --add-host unicode.org:164.132.165.173 -p 127.0.0.1:5232:22 --name=slave-priv-preprod nuxeo/jenkins-common:preprod
-ansible-playbook -i inventory/slave-priv/hosts -e "ssh_port=5232" --ssh-extra-args="-p 5232" slave-priv.yml -v
-docker commit slave-priv-preprod nuxeo/jenkins-priv:preprod
-docker kill slave-priv-preprod
-docker rm -v slave-priv-preprod
+docker run -d -t -i --add-host unicode.org:164.132.165.173 -p 127.0.0.1:2224:22 --name=slave-priv nuxeo/jenkins-common
+ansible-playbook -i inventory/slave-priv/hosts slave-priv.yml -v
+docker commit slave-priv nuxeo/jenkins-priv
+docker kill slave-priv
+docker rm -v slave-priv
 
-docker tag nuxeo/jenkins-pub:preprod dockerpriv.nuxeo.com:443/nuxeo/jenkins-slave:preprod
-docker tag nuxeo/jenkins-priv:preprod dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv:preprod
+docker run -d -t -i --add-host unicode.org:164.132.165.173 -p 127.0.0.1:2225:22 --name=slave-preprod nuxeo/jenkins-common
+ansible-playbook -i inventory/slave-preprod/hosts slave-preprod.yml -v
+docker commit slave-preprod nuxeo/jenkins-preprod
+docker kill slave-preprod
+docker rm -v slave-preprod
 
-docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-slave:preprod
-docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv:preprod
+docker tag nuxeo/jenkins-pub dockerpriv.nuxeo.com:443/nuxeo/jenkins-slave
+docker tag nuxeo/jenkins-pub dockerpriv.nuxeo.com:443/nuxeo/jenkins-it
+docker tag nuxeo/jenkins-pub dockerpriv.nuxeo.com:443/nuxeo/jenkins-multidb
+docker tag nuxeo/jenkins-pub dockerpriv.nuxeo.com:443/nuxeo/jenkins-ondemand
+docker tag nuxeo/jenkins-pub dockerpriv.nuxeo.com:443/nuxeo/jenkins-check
+docker tag nuxeo/jenkins-priv dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv
+docker tag nuxeo/jenkins-priv dockerpriv.nuxeo.com:443/nuxeo/jenkins-itpriv
+docker tag nuxeo/jenkins-preprod dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepreprod
+
+docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-slave
+docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-it
+docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-itpriv
+docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-multidb
+docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-ondemand
+docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-check
+docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv
+docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepreprod
 
 # Pull back the remote IDs
-docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-slave:preprod
-docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv:preprod
+docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-slave
+docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-it
+docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-itpriv
+docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-multidb
+docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-ondemand
+docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-check
+docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv
+docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepreprod
 
 # Docker swarm image NXBT-1770 TestAndPush
-docker build -t nuxeo/jenkins-swarm:preprod docker/swarm/
-docker tag nuxeo/jenkins-swarm:preprod dockerpriv.nuxeo.com:443/nuxeo/jenkins-swarm:preprod
-docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-swarm:preprod
-docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-swarm:preprod
+docker build -t nuxeo/jenkins-swarm docker/swarm/
+docker tag nuxeo/jenkins-swarm dockerpriv.nuxeo.com:443/nuxeo/jenkins-swarm
+docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-swarm
+docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-swarm
 
-docker images dockerpriv.nuxeo.com:443/nuxeo/*:preprod -f "since=nuxeo/jenkins-base" --format "{{.ID}} {{.Repository}}:{{.Tag}}" |tee images.list
+docker images dockerpriv.nuxeo.com:443/nuxeo/* -f "dangling=true" -q | sort -u | xargs -r docker rmi -f
+
+docker images dockerpriv.nuxeo.com:443/nuxeo/*:latest -f "since=nuxeo/jenkins-base" --format "{{.ID}} {{.Repository}}:{{.Tag}}" |tee images.list
 
