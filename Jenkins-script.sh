fcc2c3457da180c70f5554bb90cf323219c9c22c
Julien Carsique <jcarsique@nuxeo.com>
2019-01-22 17:23:57 +0100

    NXBT-2577: remove obsolete stuff (moved to nuxeo-tools-docker-priv)

--- Jenkins-script.sh
+++ Jenkins-script.sh
@@ -1,26 +1,25 @@
 #!/bin/bash -ex
+#
+# Copy from https://qa.nuxeo.org/jenkins/job/System/job/build-slave-images/
+exit 1
 
 docker login dockerpriv.nuxeo.com:443 -u $JUSER -p $JPASS
 
 echo CLEANUP
-docker kill slave-common slave-pub slave-priv slave-preprod|| true
-docker rm -v slave-common slave-pub slave-priv slave-preprod|| true
+docker kill slave-common slave-pub slave-priv || true
+docker rm -v slave-common slave-pub slave-priv || true
 
 docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv || true
-docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepreprod || true
 docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-check || true
 docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-ondemand || true
 docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-multidb || true
 docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-it || true
 docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-itpriv || true
 docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-slave || true
-docker rmi dockerpriv.nuxeo.com:443/nuxeo/jenkins-swarm || true
 docker rmi nuxeo/jenkins-priv || true
-docker rmi nuxeo/jenkins-preprod || true
 docker rmi nuxeo/jenkins-pub || true
 docker rmi nuxeo/jenkins-common || true
 docker rmi nuxeo/jenkins-base || true
-docker rmi nuxeo/jenkins-swarm || true
 docker images dockerpriv.nuxeo.com:443/nuxeo/* -f "dangling=true" -q | sort -u | xargs -r docker rmi -f
 
 
@@ -56,11 +55,6 @@ docker commit slave-priv nuxeo/jenkins-priv
 docker kill slave-priv
 docker rm -v slave-priv
 
-docker run -d -t -i --add-host unicode.org:164.132.165.173 -p 127.0.0.1:2225:22 --name=slave-preprod nuxeo/jenkins-common
-ansible-playbook -i inventory/slave-preprod/hosts slave-preprod.yml -v
-docker commit slave-preprod nuxeo/jenkins-preprod
-docker kill slave-preprod
-docker rm -v slave-preprod
 
 docker tag nuxeo/jenkins-pub dockerpriv.nuxeo.com:443/nuxeo/jenkins-slave
 docker tag nuxeo/jenkins-pub dockerpriv.nuxeo.com:443/nuxeo/jenkins-it
@@ -69,7 +63,6 @@ docker tag nuxeo/jenkins-pub dockerpriv.nuxeo.com:443/nuxeo/jenkins-ondemand
 docker tag nuxeo/jenkins-pub dockerpriv.nuxeo.com:443/nuxeo/jenkins-check
 docker tag nuxeo/jenkins-priv dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv
 docker tag nuxeo/jenkins-priv dockerpriv.nuxeo.com:443/nuxeo/jenkins-itpriv
-docker tag nuxeo/jenkins-preprod dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepreprod
 
 docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-slave
 docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-it
@@ -78,7 +71,6 @@ docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-multidb
 docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-ondemand
 docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-check
 docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv
-docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepreprod
 
 # Pull back the remote IDs
 docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-slave
@@ -88,15 +80,8 @@ docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-multidb
 docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-ondemand
 docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-check
 docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv
-docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepreprod
 
-# Docker swarm image NXBT-1770 TestAndPush
-docker build -t nuxeo/jenkins-swarm docker/swarm/
-docker tag nuxeo/jenkins-swarm dockerpriv.nuxeo.com:443/nuxeo/jenkins-swarm
-docker push dockerpriv.nuxeo.com:443/nuxeo/jenkins-swarm
-docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-swarm
 
 docker images dockerpriv.nuxeo.com:443/nuxeo/* -f "dangling=true" -q | sort -u | xargs -r docker rmi -f
 
 docker images dockerpriv.nuxeo.com:443/nuxeo/*:latest -f "since=nuxeo/jenkins-base" --format "{{.ID}} {{.Repository}}:{{.Tag}}" |tee images.list
-
