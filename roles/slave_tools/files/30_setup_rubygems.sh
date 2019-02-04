3620f62fbe2fdbe2b2e889e3e8c9e6e91521643d
Stephane Lacoin aka nxmatic <slacoin@nuxeo.com>
2019-02-04 11:22:31 +0100

    NXBT-2704 evaluate ruby path in jenkins's context

--- roles/slave_tools/files/30_setup_rubygems.sh
+++ roles/slave_tools/files/30_setup_rubygems.sh
@@ -4,7 +4,7 @@ grep -q GEM_HOME ~jenkins/.profile && exit 0
 
 sudo -H -u jenkins sh <<EOS
 set -x
-GEM_HOME="$(ruby -e 'puts Gem.user_dir')"
+GEM_HOME="\$(ruby -e 'puts Gem.user_dir')"
 cat <<EOE >> \$HOME/.profile
 # ruby gem home and path
 GEM_HOME=\$GEM_HOME; export GEM_HOME
