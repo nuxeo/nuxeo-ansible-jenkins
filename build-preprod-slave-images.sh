7be8f538e56a460bdbf8c4fbdd7fe4edfbba8219
Julien Carsique <jcarsique@nuxeo.com>
2019-05-15 18:44:46 +0200

    NXBT-2852: CasC slave images builds
    Resync with changes made in job configuration. Call script from the job.

--- build-preprod-slave-images.sh
+++ build-preprod-slave-images.sh
@@ -1,7 +1,24 @@
 #!/bin/bash -ex
 #
-# Copy from https://qa.nuxeo.org/jenkins/job/System/job/build-preprod-slave-images/
-exit 1
+# Slave images generation. Used by QA/System/build-preprod-slave-images/
+#
+# (C) Copyright 2018-2019 Nuxeo SA (http://nuxeo.com/) and contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Contributors:
+#   Alexis Timic, Mathieu Guillaume, Julien Carsique
+#
 
 docker login dockerpriv.nuxeo.com:443 -u $JUSER -p $JPASS
 
@@ -71,5 +88,7 @@ docker pull dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv:preprod
 
 
 docker images dockerpriv.nuxeo.com:443/nuxeo/*:preprod -f "since=nuxeo/jenkins-base" --format "{{.ID}} {{.Repository}}:{{.Tag}}" |tee images.list
+
+
 
 
